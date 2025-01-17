f54b1accc218a7bcb0dc1000d0e5b91379401838
PrasadRavindra2000 <wmprasadwijesinghe@gmail.com>
2025-01-17 19:27:10 +0530

    Your commit message here

--- Documents/React2/Weather/src/components/Weather.jsx
+++ Documents/React2/Weather/src/components/Weather.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Weather.css'
+import search from '../assets/search.png'
+
 
 const Weather = () => {
   return (
-    <div>
-      Weather
+    <div className='weather'>
+      
+      <div className="search-bar">
+
+        <input type="text" placeholder="Search..." />
+        <img src={search} alt="" />
+
+      </div>
+
     </div>
   )
 }
