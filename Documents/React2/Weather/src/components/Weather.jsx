baf65aa2a59d34127f4f84b15ef93a391a93f81a
PrasadRavindra2000 <wmprasadwijesinghe@gmail.com>
2025-01-17 20:22:27 +0530

    Your commit message here

--- Documents/React2/Weather/src/components/Weather.jsx
+++ Documents/React2/Weather/src/components/Weather.jsx
@@ -12,13 +12,42 @@ import wind from '../assets/wind.png'
 
 const Weather = () => {
 
+  const[ weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": clear,
+    "o1n": clear,
+    "02d": cloud,
+    "02n": cloud,
+    "03d": cloud,
+    "03n": cloud,
+    "04d": drizzle,
+    "04n": drizzle,
+    "09d": rain,
+    "09n": rain,
+    "10d": rain,
+    "10n": rain,
+    "13d": snow,
+    "13n": snow,
+  }
+
 const search = async (city)=>{
   try{
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
+
+    const icon = allIcons[data.weather[0].icon] || clear;
+
+    setWeatherData({
+      humidity: data.main.humidity,
+      windSpeed: data.wind.speed,
+      temperature: Math.floor(data.main.temp),
+      location: data.name,
+      icon: icon
+    });
   }
   catch(err){
     console.log(err)
@@ -43,15 +72,15 @@ useEffect(()=>{
 
 
     <img src={clear} alt="" className="weather-icon"/>
-    <p className='temperature'>16°C</p>
-    <p className='location'>London</p>
+    <p className='temperature'>{weatherData.temp}</p>
+    <p className='location'>{weatherData.location}</p>
 
     <div className="weather-data">
 
       <div className="col">
         <img src={humidity} alt="" />
         <div>
-        <p>91%</p>
+        <p>{weatherData.humidity}</p>
         <span>Humidity</span>
         </div>
       </div>
@@ -59,7 +88,7 @@ useEffect(()=>{
       <div className="col">
         <img src={wind} alt="" />
         <div>
-        <p>3.6Km/h</p>
+        <p>{weatherData.windSpeed}</p>
         <span>Wind Speed</span>
         </div>
       </div>
