83dd682e7623c1c267def55819dc12dd5c32976a
PrasadRavindra2000 <wmprasadwijesinghe@gmail.com>
2025-01-18 19:34:43 +0530

    Your commit message

--- Documents/React2/tic-tac-toe/src/Components/TicTacToe/TicTacToe.jsx
+++ Documents/React2/tic-tac-toe/src/Components/TicTacToe/TicTacToe.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TicTacToe.css'
 import circle from '../Assets/circle.png'
 import cross from '../Assets/cross.png'
 
+let data = ["","","","","","","","",""]
+
 const TicTacToe = () => {
+
+    let [count,setCount] = useState(0);
+    let [lock]
+
+    const toggle = (w ,num => {
+
+    }
+
+
   return (
     <div className='container'>
       <h1 className="title">Tic Tac Toe Game</h1>
 
       <div className="board">
 
+        <div className="row1">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+
+        <div className="row2">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+
+        <div className="row3">
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+            <div className="boxes"></div>
+        </div>
+
       </div>
 
       <button className="reset">Reset</button>
+
     </div>
   )
 }
